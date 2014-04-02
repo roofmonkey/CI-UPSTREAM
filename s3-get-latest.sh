6d11b278cf93a5b81fe5380b8db34cc8b1b641c3
root <root@ip-172-31-35-208.us-west-2.compute.internal>
2014-04-02 22:56:50 +0000

    Fixed minor issue with s3-get-latest.sh.  Was not appending date-time stamp to /repository

--- s3-get-latest.sh
+++ s3-get-latest.sh
@@ -5,8 +5,14 @@
 DATE=`date +%d%b%y`
 TIME=`date +%H%M`
 REPO_HOME=/home/fedora
+
 echo "[INFO] Backing up S3 Maven Repositories"
-#mv $REPO_HOME/repositories $REPO_HOME/repositories.defunct
-s3cmd get -r s3://rhbd/maven/repositories $REPO_HOME/s3-backup/ && \
-mv $REPO_HOME/s3-backup/repositories $REPO_HOME/s3-backup/repositories-$DATE.$TIME
-echo "[INFO] S3 Repository Backup Complete"
+
+#  Initiate s3 download
+s3cmd get -r s3://rhbd/maven/repositories $REPO_HOME/s3-backup/
+
+#  Append date-time stamp to repo /dir
+mv $REPO_HOME/s3-backup/repositories/ \
+   $REPO_HOME/s3-backup/repositories-$DATE.$TIME/ && \
+echo "[INFO] Appended DATE-TIME stamp to repo /dir";
+
